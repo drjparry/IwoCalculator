b78ed95af41a4b5cd9e43f31b8546b3697f19d1e
David Parry <david.parry@trustedfamily.net>
2021-10-17 12:36:55 +0100

    make calculator slightly easier to read

--- src/helpers/calculator.js
+++ src/helpers/calculator.js
@@ -1,26 +1,31 @@
 export function calculator({ date, amount, months, interestRate }) {
   let remainingPrinciple = amount;
   let totalInterest = 0;
-  const calculation = [...Array(months)].map((_, index) => {
+  const monthlyCalculation = [...Array(months)].map((_, index) => {
     const repaymentDate = date.setMonth(date.getMonth() + 1);
     const principle = amount / months;
     const interest = remainingPrinciple * (interestRate / 100);
-    totalInterest = totalInterest + interest;
     const totalRepayment = principle + interest;
-    remainingPrinciple = remainingPrinciple - principle;
 
-    return {
+    const calculation = {
       repaymentDate,
       principle,
       interest,
       totalRepayment,
     };
+
+    totalInterest = totalInterest + interest;
+    remainingPrinciple = remainingPrinciple - principle;
+
+    return calculation;
   });
 
-  calculation.push({
-    principle: amount,
-    interest: totalInterest,
-    totalRepayment: amount + totalInterest,
-  });
-  return calculation;
+  return {
+    monthlyCalculation,
+    totalCalculation: {
+      principle: amount,
+      interest: totalInterest,
+      totalRepayment: amount + totalInterest,
+    },
+  };
 }
--- src/helpers/calculator.test.js
+++ src/helpers/calculator.test.js
@@ -15,38 +15,47 @@ describe("calculator", () => {
       interestRate: 3,
     });
   });
-  test("it returns an array for each month calculated plus a total", () => {
-    expect(calculation.length).toBe(5);
+  test("it returns an array for each month calculated", () => {
+    const monthlyCalculation = calculation["monthlyCalculation"];
+    expect(monthlyCalculation.length).toBe(4);
   });
 
   test("each months array returns an object with the correct repayment date", () => {
-    const firstMonthRepaymentDate = new Date(calculation[0]["repaymentDate"]);
-    const thirdMonthRepaymentDate = new Date(calculation[2]["repaymentDate"]);
+    const monthlyCalculation = calculation["monthlyCalculation"];
+    const firstMonthRepaymentDate = new Date(
+      monthlyCalculation[0]["repaymentDate"]
+    );
+    const thirdMonthRepaymentDate = new Date(
+      monthlyCalculation[2]["repaymentDate"]
+    );
     expect(firstMonthRepaymentDate.getMonth()).toBe(11);
     expect(thirdMonthRepaymentDate.getMonth()).toBe(1);
   });
 
   test("each months array returns an object with the correct principle", () => {
-    const principle = calculation[0]["principle"];
+    const monthlyCalculation = calculation["monthlyCalculation"];
+    const principle = monthlyCalculation[0]["principle"];
     expect(principle).toBe(2500);
   });
 
   test("each months array returns an object with the months interest", () => {
-    const firstMonthsInterest = calculation[0]["interest"];
-    const thirdMonthsInterest = calculation[2]["interest"];
+    const monthlyCalculation = calculation["monthlyCalculation"];
+    const firstMonthsInterest = monthlyCalculation[0]["interest"];
+    const thirdMonthsInterest = monthlyCalculation[2]["interest"];
     expect(firstMonthsInterest).toBe(300);
     expect(thirdMonthsInterest).toBe(150);
   });
 
   test("each months array returns an object with the total repayment", () => {
-    const firstMonthsRepayment = calculation[0]["totalRepayment"];
-    const thirdMonthsRepayment = calculation[2]["totalRepayment"];
+    const monthlyCalculation = calculation["monthlyCalculation"];
+    const firstMonthsRepayment = monthlyCalculation[0]["totalRepayment"];
+    const thirdMonthsRepayment = monthlyCalculation[2]["totalRepayment"];
     expect(firstMonthsRepayment).toBe(2800);
     expect(thirdMonthsRepayment).toBe(2650);
   });
 
   test("last item in the calculation is the total principle, interest and repayment", () => {
-    const totalCalculation = calculation[4];
+    const totalCalculation = calculation["totalCalculation"];
     expect(totalCalculation["principle"]).toBe(AMOUNT);
     expect(totalCalculation["interest"]).toBe(750);
     expect(totalCalculation["totalRepayment"]).toBe(10750);
