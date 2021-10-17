8fca801dee6d797e40603b0e95c329b34c523a4b
David Parry <david.parry@trustedfamily.net>
2021-10-17 21:07:32 +0100

    loan calculation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LoanParameters from "./LoanParameters";
+import LoanCalculation from "./LoanCalculation";
 
 const LoanCalculator = () => {
   const [amountRequested, setAmountRequested] = useState(10000);
   const [duration, setDuration] = useState(4);
+
+  useEffect(() => {}, [amountRequested, duration]);
 
   return (
     <div className="container" data-test="component-app">
@@ -14,6 +17,7 @@ const LoanCalculator = () => {
         duration={duration}
         setDuration={setDuration}
       />
+      <LoanCalculation amountRequested={amountRequested} duration={duration} />
     </div>
   );
 };
---
+++ src/LoanCalculation.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { calculator } from "./helpers/calculator";
+import RepaymentTable from "./RepaymentTable";
+
+const InterestRate = ({ interestRate, setInterestRate }) => (
+  <span data-test="interest-rate-input">
+    <span>Interest rate</span>
+    <input
+      className="mb-2 mx-sm-3"
+      data-test="amount-requested-input"
+      placeholder="amount"
+      value={interestRate}
+      onChange={(event) => setInterestRate(event.target.value)}
+    ></input>
+    <span>(in %)</span>
+  </span>
+);
+
+const LoanCalculation = ({ amountRequested, duration }) => {
+  const [interestRate, setInterestRate] = useState(3);
+
+  const [calculation, setCalculation] = useState();
+
+  useEffect(() => {
+    if (amountRequested && duration && interestRate) {
+      const newCalculation = calculator({
+        date: new Date(),
+        amount: parseInt(amountRequested),
+        months: parseInt(duration),
+        interestRate: parseInt(interestRate),
+      });
+      setCalculation(newCalculation);
+    }
+  }, [amountRequested, duration, interestRate]);
+
+  return (
+    <div data-test="component-loan-calculation">
+      <InterestRate
+        interestRate={interestRate}
+        setInterestRate={setInterestRate}
+      />
+      {calculation && <RepaymentTable {...calculation} />}
+    </div>
+  );
+};
+
+export default LoanCalculation;
